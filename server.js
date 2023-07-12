3c0c84de0830a5ffd5b50be1edf75d77c7a79a15
Jason Novack <jasonnovack@Jasons-MacBook-Air.local>
2023-07-12 15:37:36 -0400

    latest updates for using a real database, the Twitter API, and securely storing user credentials

--- server.js
+++ server.js
@@ -1,28 +1,60 @@
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const AWS = require('aws-sdk');
+const bcrypt = require('bcrypt');
 const collective = require('./collective');
 // Other require statements...
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const app = express();
 
 app.use(bodyParser.json()); // For parsing application/json
-
-// In-memory "database" for users, collectives, and boost requests for the sake of this example
-let users = [];
-let collectives = [];
-let boostRequests = [];
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // ...
 
-app.post('/create-collective', collective.create);
-app.post('/join-collective', collective.join);
-app.post('/submit-tweet', collective.submitTweet);
-app.get('/boost-requests', collective.getBoostRequests);
-app.put('/boost-requests/:id', collective.updateBoostRequest);
-app.post('/set-quota', collective.setQuota);
-app.get('/quota', collective.getQuota);
-app.get('/boost-shares', collective.getBoostShares);
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+},
+function(token, tokenSecret, profile, cb) {
+    // In a real application, you'd look up the user in your database and create them if they don't exist
+    return cb(null, profile);
+}
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+passport.authenticate('twitter', { failureRedirect: '/login' }),
+function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
+
 // Other routes...
 
 app.listen(3000, function () {
