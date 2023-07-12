1d7795947781a4d219d48e479a33ecf8dbdc7d1a
Jason Novack <jasonnovack@Jasons-MacBook-Air.local>
2023-07-12 15:24:57 -0400

    add the feature that allows users to see how many boost shares they have

--- collective.js
+++ collective.js
@@ -3,12 +3,7 @@
 
 // ...
 
-exports.setQuota = function(req, res) {
-    // In a real application, you'd validate the input and set the user's quota in your database
-    res.send('Quota set');
-}
-
-exports.getQuota = function(req, res) {
-    // In a real application, you'd get the user's quota from your database
-    res.send('Quota');
+exports.getBoostShares = function(req, res) {
+    // In a real application, you'd get the user's boost shares from your database
+    res.send('Boost shares');
 }
--- server.js
+++ server.js
@@ -13,23 +13,7 @@ let users = [];
 let collectives = [];
 let boostRequests = [];
 
-// User signup handler
-app.post('/signup', (req, res) => {
-    // In a real application, you'd validate the input and securely store the user in your database
-    users.push(req.body);
-    res.status(201).send('User created');
-});
-
-// User signin handler
-app.post('/signin', (req, res) => {
-    // In a real application, you'd validate the input and check the user's credentials in your database
-    const user = users.find(user => user.username === req.body.username && user.password === req.body.password);
-    if (user) {
-        res.send('User signed in');
-    } else {
-        res.status(401).send('Invalid credentials');
-    }
-});
+// ...
 
 app.post('/create-collective', collective.create);
 app.post('/join-collective', collective.join);
@@ -38,6 +22,7 @@ app.get('/boost-requests', collective.getBoostRequests);
 app.put('/boost-requests/:id', collective.updateBoostRequest);
 app.post('/set-quota', collective.setQuota);
 app.get('/quota', collective.getQuota);
+app.get('/boost-shares', collective.getBoostShares);
 // Other routes...
 
 app.listen(3000, function () {
